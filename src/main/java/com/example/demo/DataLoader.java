889ba67eef9dc65fe6508d8e5ce72c4b51aec497
Irina <irinamiranda@github.com>
2018-11-15 12:01:30 -0500

    Pre-load some data

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -29,11 +29,7 @@ public class DataLoader implements CommandLineRunner {
         Role adminRole = roleRepository.findByRole("ADMIN");
         Role userRole = roleRepository.findByRole("USER");
 
-        User user = new User("jim@jim.com", passwordEncoder.encode("password"), "Jim", "Jimmerson", true, "jim", "VIP user");
-        user.setRoles(Arrays.asList(userRole));
-        userRepository.save(user);
-
-        user = new User("admin@admin.com", passwordEncoder.encode("password"), "Admin", "User", true, "admin", "Site Super Admin");
+        User user = new User("admin@admin.com", passwordEncoder.encode("password"), "Admin", "User", true, "admin", "Site Super Admin");
         user.setRoles(Arrays.asList(adminRole));
         userRepository.save(user);
 
@@ -41,6 +37,18 @@ public class DataLoader implements CommandLineRunner {
         message.setOwner(user);
         message.setText("Welcome Message from Admin");
         message.setDate("2018-11-01");
+        message.setHashtags("info,welcome");
+        messageRepository.save(message);
+
+        user = new User("study.javaclass@gmail.com", passwordEncoder.encode("password"), "Jim", "Jimmerson", true, "jim", "VIP user");
+        user.setRoles(Arrays.asList(userRole));
+        userRepository.save(user);
+
+        message = new Message();
+        message.setOwner(user);
+        message.setText("Hi, this is message from Jim!");
+        message.setDate("2018-11-01");
+        message.setHashtags("info,welcome,jim's tag 1");
         messageRepository.save(message);
     }
 }
